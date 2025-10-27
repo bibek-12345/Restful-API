bf092d9346bd1d52d575d65d9ef2aed604de0814
Bibek Bhattarai <bibek.bhattarai072@gmail.com>
2025-10-27 17:10:07 -0400

    get api

---
+++ Get_API/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const path = require('path');
+
+//using middleware for parsing the data send through form submition
+app.use(express.urlencoded({extended:true}));
+
+// path for views(ejs)
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+//for static file (public) path
+app.use(express.static(path.join(__dirname, "/public")));
+
+//create array to store data
+let posts = [
+    {
+        username : "lambtonollege",
+        content : "I love coding"
+    },
+    {
+        username : "Bibek",
+        content : "Hard work is important to achieve success"
+    },
+    {
+        username : "Rahul",
+        content : "I got my first internship"
+    },
+];
+
+app.get("/posts", (req,res)=>{
+    res.render("index.ejs",{posts});
+});
+
+
+app.listen(port, ()=>{
+    console.log("server is listening to port 8080");
+});
